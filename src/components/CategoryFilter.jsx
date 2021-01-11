9c41e1343d644552375c778c0ea27912d157826e
somoye123 <somoye.ayotunde@gmail.com>
2021-01-11 22:55:02 +0100

    add application category filter component

---
+++ src/components/CategoryFilter.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Categories = ['Male', 'Female', 'MasterCard', 'Visa', 'paypal', 'check'];
+
+const CategoryFilter = ({ handleFilterChange }) => (
+  <select onChange={handleFilterChange}>
+    <option value="ALL">ALL</option>
+    {Categories.map((category) => (
+      <option value={category} key={category}>
+        {category}
+      </option>
+    ))}
+  </select>
+);
+
+CategoryFilter.propTypes = {
+  handleFilterChange: PropTypes.func.isRequired,
+};
+
+export default CategoryFilter;
