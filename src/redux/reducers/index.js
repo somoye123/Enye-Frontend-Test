33b8973590be72b6498a0e13b336026ec93d1fa4
somoye123 <somoye.ayotunde@gmail.com>
2021-01-12 17:34:30 +0100

    add searchTerm to application combined reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import RawProfiles from './rawProfilesReducer';
 import Profiles from './profilesReducer';
 import Loading from './loadingReducer';
+import SearchTerm from './searchTermReducer';
 
 export default combineReducers({
   Profiles,
   Loading,
   RawProfiles,
+  SearchTerm,
 });
