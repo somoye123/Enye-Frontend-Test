edc3d9da5ca5b237f6e66354a4804ec7e9cc7786
somoye123 <somoye.ayotunde@gmail.com>
2021-01-12 18:38:53 +0100

    build application searchTerm action creator

--- src/redux/actions/searchTermAction.js
+++ src/redux/actions/searchTermAction.js
@@ -1,8 +1,21 @@
 import * as types from './actionTypes';
+import paginate from '../../utils/paginate';
 
-const searchTermAction = (searchTerm) => ({
-  type: types.SET_SEARCH_TERM,
-  searchTerm,
-});
+const searchTermAction = (rawProfiles, searchTerm) => {
+  let FilteredPagination;
+  searchTerm === ''
+    ? (FilteredPagination = paginate(rawProfiles))
+    : (FilteredPagination = paginate(
+        rawProfiles.filter(
+          (profile) =>
+            profile.FirstName.includes(searchTerm) ||
+            profile.LastName.includes(searchTerm)
+        )
+      ));
+  return {
+    type: types.SET_PROFILES,
+    profiles: FilteredPagination,
+  };
+};
 
 export default searchTermAction;
