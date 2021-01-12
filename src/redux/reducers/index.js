44b1ef7a0bad08a767e32a8627d986021f74699c
somoye123 <somoye.ayotunde@gmail.com>
2021-01-12 18:39:51 +0100

    remove SearchTerm reducer from combined reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,9 @@ import { combineReducers } from 'redux';
 import RawProfiles from './rawProfilesReducer';
 import Profiles from './profilesReducer';
 import Loading from './loadingReducer';
-import SearchTerm from './searchTermReducer';
 
 export default combineReducers({
   Profiles,
   Loading,
   RawProfiles,
-  SearchTerm,
 });
